01b918263bcb55fecf1e1e20cee6925a57fff867
HibbaHindawi <hibba04@hotmail.se>
2024-02-02 19:56:14 +0100

    yuh

--- U3/js/script.js
+++ U3/js/script.js
@@ -14,6 +14,42 @@ window.addEventListener("load", init); // init aktiveras då sidan är inladdad
 // --------------------------------------------------
 // Avläs menyn för val av ämne
 function selectSubject() {
-    
+    let subject = this.value;
+    requestData(subject);
+    this.selectedIndex = 0;
 } // Slut selectSubject
+
+function requestData (subject){
+    let request = new XMLHttpRequest();
+    request.onload = function () {
+        if (request.status == 200) getData(request.responseXML, subject);
+        else subjectInfoElem.innerText = "Fel vid hämtning: " + request.status;
+    }
+    request.open("GET", "data/subjects.xml", true);
+    request.send()
+}
+function getData(XMLcode, subject) {
+    let subjectElem = XMLcode.querySelectorAll("subject");
+    let falseMessage = XMLcode.querySelector("not_awailable");
+    let HTMLcode = "";
+    let subjectFound = false;
+
+    for (let j = 0; j < subjectElem.length; j++) {
+        if (subjectElem[j].querySelector("name").textContent === subject) {
+            let s = subjectElem[j];
+            let name = s.querySelector("name").textContent;
+            let info = s.querySelector("info").textContent;
+            subjectFound = true;
+
+            HTMLcode += "<h3>" + name + "</h3>";
+            HTMLcode += "<p>" + info + "</p>";
+            break;
+        }
+    }
+
+    if (!subjectFound) {
+        HTMLcode = "<p>" + falseMessage.textContent + "</p>";
+    }
+    subjectInfoElem.innerHTML = HTMLcode;
+}
 // --------------------------------------------------
